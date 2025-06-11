32c5808255e81531c4fe153c14dc0a72bc282a73
kiegoon <qkqhdmlskf061@gmail.com>
2025-06-11 22:45:35 +0900

    Create RpgMod.java

---
+++ src/main/java/com/example/rpgmod/RpgMod.java
@@ -0,0 +1,10 @@
+package com.example.rpgmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RpgMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("🌟 RPG Mod Loaded!");
+    }
+}
